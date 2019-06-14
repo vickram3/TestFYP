114a38bb20f8358e7cd1c6299ca550c8dc13cc24
vickneshram25 <51814999+vickneshram25@users.noreply.github.com>
2019-06-14 16:13:51 +0800

    Update and rename keypair.js to keypair.py

--- src/model/keypair.js
+++
@@ -1,9 +0,0 @@
-class KeyPair{
-
-    constructor(projectId, apiKey){
-        this.apiKey = apiKey;
-        this.projectId = projectId;
-    }
-}
-
-module.exports.default = KeyPair;
---
+++ src/model/keypair.py
@@ -0,0 +1,6 @@
+class KeyPair:
+
+    #Constructor
+    def __init__(self, projectId, apiKey):
+        self.apiKey = apiKey
+        self.projectId = projectId
